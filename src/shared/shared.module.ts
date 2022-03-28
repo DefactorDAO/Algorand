04b78ac1e3cf8d832bb5af77b2c68c31d94d57e2
Aseem PM <aseem@phaethon.tech>
2022-03-28 13:49:21 +0530

    created algorandMo

--- src/shared/shared.module.ts
+++ src/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { IpfsModule } from './ipfs/ipfs.module';
+import { AlgorandModule } from './algorand/algorand.module';
 
 @Module({
-  imports: [IpfsModule]
+  imports: [IpfsModule, AlgorandModule]
 })
 export class SharedModule {}
