5e3b24d09444bd48afdc611bd2e06b35f17aeaae
Aseem PM <aseem@phaethon.tech>
2022-03-28 20:58:36 +0530

    created documentModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MintModule } from './mint/mint.module';
+import { DocumentModule } from './document/document.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MintModule],
+  imports: [ConfigModule.forRoot(), MintModule, DocumentModule],
   controllers: [AppController],
   providers: [AppService],
 })
