dd5da410d0f9a71f33b357a61272cc8b5652db5f
Aseem PM <aseem@phaethon.tech>
2022-03-28 13:47:28 +0530

    added pintJsonToIpfs Me - ifpsService

--- src/shared/ipfs/ipfs.service.ts
+++ src/shared/ipfs/ipfs.service.ts
@@ -1,4 +1,23 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
-export class IpfsService {}
+export class IpfsService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async pinJsonToIpfs(json): Promise<AxiosResponse> {
+    const httpObs = this.httpService.post(
+      'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+      json,
+      {
+        headers: {
+          pinata_api_key: process.env.PINATA_API_KEY,
+          pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
+        },
+      },
+    );
+    return (await lastValueFrom(httpObs)).data;
+  }
+}
