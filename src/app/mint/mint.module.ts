fbcd84c35a2df5808f779d56dd38a38e263b7bac
Aseem PM <aseem@phaethon.tech>
2022-03-28 16:39:06 +0530

    imported algoMo - mintModule

--- src/app/mint/mint.module.ts
+++ src/app/mint/mint.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AlgorandModule } from 'src/shared/algorand/algorand.module';
 import { IpfsModule } from 'src/shared/ipfs/ipfs.module';
 import { MintController } from './mint.controller';
 import { MintService } from './mint.service';
 
 @Module({
-  imports: [IpfsModule],
+  imports: [IpfsModule, AlgorandModule],
   controllers: [MintController],
   providers: [MintService],
 })
