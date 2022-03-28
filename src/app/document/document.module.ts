45b12855c987022beae574cd35abee6f9613ba16
Aseem PM <aseem@phaethon.tech>
2022-03-28 20:59:25 +0530

    created documentService

--- src/app/document/document.module.ts
+++ src/app/document/document.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { DocumentController } from './document.controller';
+import { DocumentService } from './document.service';
 
 @Module({
-  controllers: [DocumentController]
+  controllers: [DocumentController],
+  providers: [DocumentService]
 })
 export class DocumentModule {}
