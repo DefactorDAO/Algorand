549918221bd1fb09dbf7f17037919d6241656d63
Aseem PM <aseem@phaethon.tech>
2022-03-28 16:39:56 +0530

    fetched account & pinataReponse - mintServ

--- src/app/mint/mint.service.ts
+++ src/app/mint/mint.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { AccountService } from 'src/shared/algorand/account/account.service';
 import { IpfsService } from 'src/shared/ipfs/ipfs.service';
 
 @Injectable()
 export class MintService {
-  constructor(private readonly ipfsService: IpfsService) {}
+  constructor(
+    private readonly ipfsService: IpfsService,
+    private readonly accountService: AccountService,
+  ) {}
 
   async mint(data: any) {
-    return this.ipfsService.pinJsonToIpfs(data);
+    const account = this.accountService.getAccountFromEnvMnemonic();
+    const pinataResponse = await this.ipfsService.pinJsonToIpfs(data);
   }
 }
