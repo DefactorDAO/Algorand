b81619634bd285c501f5ac925f09cf5e6e9c6195
Aseem PM <aseem@phaethon.tech>
2022-03-28 13:02:20 +0530

    added http module

--- src/app/mint/mint.module.ts
+++ src/app/mint/mint.module.ts
@@ -1,9 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { MintController } from './mint.controller';
 import { MintService } from './mint.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [MintController],
-  providers: [MintService]
+  providers: [MintService],
 })
 export class MintModule {}
