caf4c511f8e9bffcd7378ba654139c5ce2a19ab0
Aseem PM <aseem@phaethon.tech>
2022-03-28 13:34:08 +0530

    added post mintCo

--- src/app/mint/mint.controller.ts
+++ src/app/mint/mint.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Version } from '@nestjs/common';
+import { MintService } from './mint.service';
 
 @Controller('mint')
-export class MintController {}
+export class MintController {
+  constructor(private readonly mintService: MintService) {}
+
+  @Version('1')
+  @Post()
+  mint(@Body() data: any) {
+    return this.mintService.mint(data);
+  }
+}
