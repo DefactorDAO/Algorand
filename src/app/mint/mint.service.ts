66254bf4706a0aeea8ba110c9d51395b8d1c9e02
Aseem PM <aseem@phaethon.tech>
2022-03-28 13:33:47 +0530

    added pinToIpfs method - mintSe

--- src/app/mint/mint.service.ts
+++ src/app/mint/mint.service.ts
@@ -1,4 +1,29 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable()
-export class MintService {}
+export class MintService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async mint(data: any) {
+    const ipfsUrl = await this.pinToIpfs(data);
+    return ipfsUrl;
+  }
+
+  private async pinToIpfs(data) {
+    const httpObs = this.httpService.post(
+      'https://api.pinata.cloud/pinning/pinJSONToIPFS',
+      data,
+      {
+        headers: {
+          pinata_api_key: process.env.PINATA_API_KEY,
+          pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
+        },
+      },
+    );
+    const pinataRes = await lastValueFrom(httpObs);
+    const url = `${process.env.PINATA_API_GATEWAY}/ipfs/${pinataRes['data']['IpfsHash']}`;
+    return url;
+  }
+}
