68726f01b5cb775f78c1bbd985cd3c339b2fa932
Aseem PM <aseem@phaethon.tech>
2022-03-28 13:48:40 +0530

    decoupled ipfsService

--- src/app/mint/mint.service.ts
+++ src/app/mint/mint.service.ts
@@ -1,29 +1,11 @@
-import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { lastValueFrom } from 'rxjs';
+import { IpfsService } from 'src/shared/ipfs/ipfs.service';
 
 @Injectable()
 export class MintService {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(private readonly ipfsService: IpfsService) {}
 
   async mint(data: any) {
-    const ipfsUrl = await this.pinToIpfs(data);
-    return ipfsUrl;
-  }
-
-  private async pinToIpfs(data) {
-    const httpObs = this.httpService.post(
-      'https://api.pinata.cloud/pinning/pinJSONToIPFS',
-      data,
-      {
-        headers: {
-          pinata_api_key: process.env.PINATA_API_KEY,
-          pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
-        },
-      },
-    );
-    const pinataRes = await lastValueFrom(httpObs);
-    const url = `${process.env.PINATA_API_GATEWAY}/ipfs/${pinataRes['data']['IpfsHash']}`;
-    return url;
+    return this.ipfsService.pinJsonToIpfs(data);
   }
 }
