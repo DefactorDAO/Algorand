a8df44d1087b90d4bd363fd18704745bb2e67b6c
Aseem PM <aseem@phaethon.tech>
2022-03-29 14:01:21 +0530

    modified to modular & added pinToFile - ipfsServ

--- src/shared/ipfs/ipfs.service.ts
+++ src/shared/ipfs/ipfs.service.ts
@@ -1,22 +1,43 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { AxiosRequestConfig } from 'axios';
 import { lastValueFrom } from 'rxjs';
 import { PinataResponseDto } from './dto/pinata-response.dto';
+import { PinataPins } from './enum/pinata-endpoint.enum';
 
 @Injectable()
 export class IpfsService {
+  private endPoint = 'https://api.pinata.cloud/pinning';
+
   constructor(private readonly httpService: HttpService) {}
 
-  async pinJsonToIpfs(json): Promise<PinataResponseDto> {
-    const httpObs = this.httpService.post(
-      'https://api.pinata.cloud/pinning/pinJSONToIPFS',
-      json,
-      {
-        headers: {
-          pinata_api_key: process.env.PINATA_API_KEY,
-          pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
-        },
+  async pinJsonToIpfs(json) {
+    return this.pinataRequest(PinataPins.JSON_TO_IPFS, json);
+  }
+
+  async pinFileToIPFS(file) {
+    return this.pinataRequest(PinataPins.FILE_TO_IPFS, file);
+  }
+
+  // ----------- non controller methods -------------
+
+  private async pinataRequest(uri, data): Promise<PinataResponseDto> {
+    const config: AxiosRequestConfig = {
+      headers: {
+        pinata_api_key: process.env.PINATA_API_KEY,
+        pinata_secret_api_key: process.env.PINATA_SECRET_API_KEY,
       },
+    };
+    if (uri == PinataPins.FILE_TO_IPFS) {
+      config.maxContentLength = -1;
+      config.headers[
+        'Content-Type'
+      ] = `multipart/form-data; boundary=${data?.['_boundary']}`;
+    }
+    const httpObs = this.httpService.post(
+      `${this.endPoint}/${uri}`,
+      data,
+      config,
     );
     return (await lastValueFrom(httpObs)).data;
   }
