7615c4a9a1f34e3aa786eb4142bfa3222fbb89da
Aseem PM <aseem@phaethon.tech>
2022-03-29 16:44:31 +0530

    added global prefix api - main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import swaggerConfig from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
   });
