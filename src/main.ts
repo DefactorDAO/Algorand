e65cd92f6ec811398ad0ccd47ce793321b490632
Aseem PM <aseem@phaethon.tech>
2022-03-29 14:09:46 +0530

    added swaggerConfig - main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import swaggerConfig from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
   });
+  swaggerConfig(app);
   await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
