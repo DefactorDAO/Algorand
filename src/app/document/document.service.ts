08052d423104c3185433fe61dd0879b3e1d81138
Aseem PM <aseem@phaethon.tech>
2022-03-29 14:02:33 +0530

    bundling file for ipfs - documentServi

--- src/app/document/document.service.ts
+++ src/app/document/document.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import * as FormData from 'form-data';
+import { IpfsService } from 'src/shared/ipfs/ipfs.service';
 
 @Injectable()
-export class DocumentService {}
+export class DocumentService {
+  constructor(private readonly ipfsService: IpfsService) {}
+
+  async uploadToIpfs(files: Express.Multer.File[]) {
+    const data = new FormData();
+
+    for (let i = 0; i < files.length; i++) {
+      const fileContent = Buffer.from(files[i].buffer);
+      data.append('file', fileContent as any, {
+        filepath: `files/${files[i].originalname}`,
+      });
+    }
+
+    return this.ipfsService.pinFileToIPFS(data);
+  }
+}
