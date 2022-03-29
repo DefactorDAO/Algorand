3ce243d55d55c91225ed1208a72e261b9057ef16
Aseem PM <aseem@phaethon.tech>
2022-03-29 14:03:25 +0530

    added endPoint to multiple files - documentCo

--- src/app/document/document.controller.ts
+++ src/app/document/document.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UploadedFiles,
+  UseInterceptors,
+  Version,
+} from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { DocumentService } from './document.service';
 
 @Controller('document')
-export class DocumentController {}
+export class DocumentController {
+  constructor(private readonly documentService: DocumentService) {}
+
+  @Version('1')
+  @Post('ipfs')
+  @UseInterceptors(FilesInterceptor('documents'))
+  uploadToIpfs(@UploadedFiles() documents) {
+    return this.documentService.uploadToIpfs(documents);
+  }
+}
