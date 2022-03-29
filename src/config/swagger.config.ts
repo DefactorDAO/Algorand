9f51b953b3ac9354da520d8acda1a4767aa7c902
Aseem PM <aseem@phaethon.tech>
2022-03-29 16:49:59 +0530

    added swagger versioning

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -4,6 +4,7 @@ export default (app) => {
   const swaggerConfig = new DocumentBuilder()
     .setTitle('Algorand PoC')
     .setDescription('Algorand PoC')
+    .setVersion(process.env.npm_package_version)
     .build();
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('swagger', app, document);
