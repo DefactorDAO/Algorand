5508ff1c2ecb7506d5fb85ccaea0bf3134882506
Aseem PM <aseem@phaethon.tech>
2022-03-29 14:01:48 +0530

    imported ipfsModule - documentMo

--- src/app/document/document.module.ts
+++ src/app/document/document.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { IpfsModule } from 'src/shared/ipfs/ipfs.module';
 import { DocumentController } from './document.controller';
 import { DocumentService } from './document.service';
 
 @Module({
+  imports: [IpfsModule],
   controllers: [DocumentController],
-  providers: [DocumentService]
+  providers: [DocumentService],
 })
 export class DocumentModule {}
