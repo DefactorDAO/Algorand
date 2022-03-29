f787199d488276d4d2144bebecf03be2fe26ad03
Aseem PM <aseem@phaethon.tech>
2022-03-29 14:18:32 +0530

    documented ipfs file upload - documentCo

--- src/app/document/document.controller.ts
+++ src/app/document/document.controller.ts
@@ -6,14 +6,20 @@ import {
   Version,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { ApiBody, ApiConsumes, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { DocumentService } from './document.service';
+import { FilesUploadDto } from './dto/files-upload.dto';
 
+@ApiTags('Document')
 @Controller('document')
 export class DocumentController {
   constructor(private readonly documentService: DocumentService) {}
 
   @Version('1')
   @Post('ipfs')
+  @ApiOperation({ summary: 'uploading multiple documents to IPFS' })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({ type: FilesUploadDto })
   @UseInterceptors(FilesInterceptor('documents'))
   uploadToIpfs(@UploadedFiles() documents) {
     return this.documentService.uploadToIpfs(documents);
