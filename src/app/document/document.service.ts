5b8ea8c3c49e3ba45e17d537a5c77965a07e6bbb
Aseem PM <aseem@phaethon.tech>
2022-03-30 11:50:25 +0530

    simplified the documentService

--- src/app/document/document.service.ts
+++ src/app/document/document.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import * as FormData from 'form-data';
 import { IpfsService } from 'src/shared/ipfs/ipfs.service';
 
 @Injectable()
@@ -7,15 +6,6 @@ export class DocumentService {
   constructor(private readonly ipfsService: IpfsService) {}
 
   async uploadToIpfs(files: Express.Multer.File[]) {
-    const data = new FormData();
-
-    for (let i = 0; i < files.length; i++) {
-      const fileContent = Buffer.from(files[i].buffer);
-      data.append('file', fileContent as any, {
-        filepath: `files/${files[i].originalname}`,
-      });
-    }
-
-    return this.ipfsService.pinFileToIPFS(data);
+    return this.ipfsService.pinFilesToIPFS(files);
   }
 }
