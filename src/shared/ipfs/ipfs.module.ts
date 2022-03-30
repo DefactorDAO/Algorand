06fd1b22ca0588a62d34f281fa02b632b20531e6
Aseem PM <aseem@phaethon.tech>
2022-03-30 12:37:37 +0530

    imported aes module

--- src/shared/ipfs/ipfs.module.ts
+++ src/shared/ipfs/ipfs.module.ts
@@ -1,9 +1,10 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
+import { AesModule } from '../aes/aes.module';
 import { IpfsService } from './ipfs.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AesModule],
   providers: [IpfsService],
   exports: [IpfsService],
 })
