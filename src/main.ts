d68406a4e59776937658911252b52d71e06917af
Aseem PM <aseem@phaethon.tech>
2022-04-05 13:02:31 +0530

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import swaggerConfig from './config/swagger.config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors({ origin: process.env.CORS });
   app.enableVersioning({
     type: VersioningType.URI,
   });
