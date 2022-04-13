b9f78f07be705c0f25d0ea6801f10740411e8037
Aseem PM <aseem@phaethon.tech>
2022-04-13 13:55:32 +0530

    added get and getByAssetId - nftCo

--- src/app/nft/nft.controller.ts
+++ src/app/nft/nft.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Version } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { NftService } from './nft.service';
 
+@ApiTags('NFT')
 @Controller('nft')
-export class NftController {}
+export class NftController {
+  constructor(private readonly nftService: NftService) {}
+
+  @Version('1')
+  @Get()
+  get() {
+    return this.nftService.get();
+  }
+
+  @Version('1')
+  @Get(':assetId')
+  getByAssetId(@Param('assetId') assetId: string) {
+    return this.nftService.getByAssetId(assetId);
+  }
+}
