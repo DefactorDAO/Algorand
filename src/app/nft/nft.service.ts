0c5e43e6c87746ab8236cc5f4830df2ea4097d5e
Aseem PM <aseem@phaethon.tech>
2022-04-13 13:54:55 +0530

    added updated get & getByAssetId - nftSer

--- src/app/nft/nft.service.ts
+++ src/app/nft/nft.service.ts
@@ -1,4 +1,39 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { lastValueFrom } from 'rxjs';
+import { AccountService } from 'src/shared/algorand/account/account.service';
+import { AlgorandService } from 'src/shared/algorand/algorand.service';
 
 @Injectable()
-export class NftService {}
+export class NftService {
+  constructor(
+    private readonly accountService: AccountService,
+    private readonly algorandService: AlgorandService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  async get() {
+    const algoClient = this.algorandService.client();
+    const account = this.accountService.getAccountFromEnvMnemonic();
+    const accountInfo = await algoClient.accountInformation(account.addr).do();
+    return accountInfo['created-assets'];
+  }
+
+  async getByAssetId(id: string) {
+    const algoClient = this.algorandService.client();
+    const account = this.accountService.getAccountFromEnvMnemonic();
+    const assetInfo = await algoClient
+      .accountAssetInformation(account.addr, parseInt(id))
+      .do();
+    const metadata = (
+      await lastValueFrom(
+        this.httpService.get(assetInfo?.['created-asset']?.url),
+      )
+    ).data;
+    const asset = {
+      ...assetInfo['created-asset'],
+      metadata,
+    };
+    return asset;
+  }
+}
